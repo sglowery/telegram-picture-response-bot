23a0ddcdb44d9758dc689048bc6cf9a2a9f709d4
sglowery <stephen.g.lowery@gmail.com>
2019-08-06 21:52:03 -0600

    Cleaned up unused string constants

--- pictureresponsebot/config example.py
+++ pictureresponsebot/config example.py
@@ -1,8 +1,6 @@
 NEW_USER_REPLY = "Hi {new_member_name}! Welcome to {chat_name}. Enjoy a complimentary image, on the house."
-LOG_NEW_USER_REPLY = "new user"
 COMMAND_REPLY = "Why hello there. Have you seen this picture? Now you have."
-LOG_COMMAND_REPLY = "picture command called"
-COMMAND_NAME = "picture"
+SEND_PICTURE_COMMAND = "picture"
 
 IMAGE_DIRECTORY = "pictureresponsebot/assets/"
 IMAGE_NAME = "response.jpg"
