448978c731b7ec4a0d3bb39c9324d7acd16f5ae5
sglowery <stephen.g.lowery@gmail.com>
2019-09-20 20:16:26 -0600

    refactored a bunch

--- pictureresponsebot/PictureResponseBot.py
+++ pictureresponsebot/PictureResponseBot.py
@@ -1,14 +1,13 @@
 import logging
-from typing import NoReturn
+from typing import NoReturn, Iterable, Dict, Any
 
-from telegram import Bot, Update
+from telegram import Bot, Update, User
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, Dispatcher
 
 import pictureresponsebot.config as config
 from pictureresponsebot.util import call_and_log, bot_send_picture
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -29,19 +28,26 @@ class PictureResponseBot:
 
     @bot_send_picture
     @call_and_log
-    def respond_to_new_members(self, bot: Bot, update: Update) -> dict:
-        human_users = list(filter(lambda user: not user.is_bot, update.message.new_chat_members))
+    def respond_to_new_members(self, bot: Bot, update: Update) -> Dict[str, Any]:
+        human_users: Iterable[User] = self._get_human_users(update.message.new_chat_members)
         response_text: str = ", ".join([user.name for user in human_users])
         welcome_chat: str = config.NEW_USER_REPLY.format(new_member_names=response_text,
                                                          chat_name=update.message.chat.title)
-        return {'self': bot,
-                'chat_id': update.message.chat.id,
-                'caption': welcome_chat}
+        return self._construct_message_object(bot, update.message.chat.id, welcome_chat)
 
     @bot_send_picture
     @call_and_log
-    def send_picture_from_command(self, bot: Bot, update: Update) -> dict:
-        return {'self': bot,
-                'chat_id': update.message.chat.id,
-                'caption': config.COMMAND_REPLY,
-                'reply_id': update.message.message_id}
+    def send_picture_from_command(self, bot: Bot, update: Update) -> Dict[str, Any]:
+        return self._construct_message_object(bot, update.message.chat.id, config.COMMAND_REPLY,
+                                              update.message.message_id)
+
+    @staticmethod
+    def _construct_message_object(from_bot: Bot, chat_id: int, caption: str, reply_id: int = None) -> Dict[str, Any]:
+        return {'self': from_bot,
+                'chat_id': chat_id,
+                'caption': caption,
+                'reply_id': reply_id}
+
+    @staticmethod
+    def _get_human_users(user_list: Iterable[User]) -> Iterable[User]:
+        return [user for user in user_list if not user.is_bot]
--- pictureresponsebot/util.py
+++ pictureresponsebot/util.py
@@ -1,5 +1,5 @@
 import logging as logger
-from typing import Callable, NoReturn, Any, Optional
+from typing import Callable, NoReturn, Any, Optional, Dict
 
 from telegram import Bot
 
@@ -8,21 +8,27 @@ from pictureresponsebot import config as config
 
 def call_and_log(func: Callable) -> Callable:
     def _wrapped(*args, **kwargs) -> Optional[Any]:
-        log: str = " ".join((func.__name__, "called"))
+        log: str = f"{func.__name__} called"
         logger.info(log)
         return func(*args, **kwargs)
+
     return _wrapped
 
 
 def bot_send_picture(func: Callable) -> Callable:
     def _wrapped(*args, **kwargs) -> NoReturn:
-        payload: dict = func(*args, **kwargs)
-        send_photo_args: dict = {**payload,
-                                 'photo': open(construct_image_path(), 'rb')
-                                 }
+        payload: Dict[str, Any] = func(*args, **kwargs)
+        send_photo_args: Dict[str, Any] = {**payload,
+                                           'photo': open(get_image_path(), 'rb')
+                                           }
         Bot.send_photo(**send_photo_args)
+
     return _wrapped
 
 
-def construct_image_path():
-    return ''.join((config.IMAGE_DIRECTORY, config.IMAGE_NAME))
+def get_image_path() -> str:
+    return f"{config.IMAGE_DIRECTORY}{config.IMAGE_NAME}"
+
+
+def clamp(num: int, lower: int, upper: int) -> int:
+    return lower if num < lower else upper if num > lower else num
