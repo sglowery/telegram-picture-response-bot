99aaf0c5f6f0db8d16371d10ebadc60c460456ad
sglowery <stephen.g.lowery@gmail.com>
2019-10-26 11:12:21 -0600

    updated handler code for version 12 of python-telegram-bot, did some more refactoring and code clean-up

--- pictureresponsebot/PictureResponseBot.py
+++ pictureresponsebot/PictureResponseBot.py
@@ -1,8 +1,8 @@
 import logging
-from typing import NoReturn, Iterable, Dict, Any
+from typing import NoReturn, Iterable, Dict, List
 
-from telegram import Bot, Update, User
-from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, Dispatcher
+from telegram import Bot, Update, User, Message
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, Dispatcher, CallbackContext
 
 import pictureresponsebot.config as config
 from pictureresponsebot.util import call_and_log, bot_send_picture
@@ -11,6 +11,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+TelegramMessageArgs = Dict[str, Bot or int or str]
+
 
 class PictureResponseBot:
 
@@ -18,36 +20,36 @@ class PictureResponseBot:
         self.token: str = token
 
     def run(self) -> NoReturn:
-        updater: Updater = Updater(self.token)
+        updater: Updater = Updater(self.token, use_context=True)
         dp: Dispatcher = updater.dispatcher
-        dp.add_handler(CommandHandler(config.COMMAND_NAME, self.send_picture_from_command))
-        dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, self.respond_to_new_members))
+        dp.add_handler(CommandHandler(config.COMMAND_NAME, self._send_picture_from_command))
+        dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, self._respond_to_new_members))
         updater.start_polling()
         logging.info("bot is running")
         updater.idle()
 
     @bot_send_picture
     @call_and_log
-    def respond_to_new_members(self, bot: Bot, update: Update) -> Dict[str, Any]:
-        human_users: Iterable[User] = self._get_human_users(update.message.new_chat_members)
-        response_text: str = ", ".join([user.name for user in human_users])
-        welcome_chat: str = config.NEW_USER_REPLY.format(new_member_names=response_text,
-                                                         chat_name=update.message.chat.title)
-        return self._construct_message_object(bot, update.message.chat.id, welcome_chat)
+    def _respond_to_new_members(self, update: Update, context: CallbackContext) -> TelegramMessageArgs:
+        return self._construct_message_object(context.bot, update.message.chat.id, self._get_welcome_message(update.message))
 
     @bot_send_picture
     @call_and_log
-    def send_picture_from_command(self, bot: Bot, update: Update) -> Dict[str, Any]:
-        return self._construct_message_object(bot, update.message.chat.id, config.COMMAND_REPLY,
-                                              update.message.message_id)
+    def _send_picture_from_command(self, update: Update, context: CallbackContext) -> TelegramMessageArgs:
+        return self._construct_message_object(context.bot, update.message.chat.id, config.COMMAND_REPLY, update.message.message_id)
 
-    @staticmethod
-    def _construct_message_object(from_bot: Bot, chat_id: int, caption: str, reply_id: int = None) -> Dict[str, Any]:
+    def _construct_message_object(self, from_bot: Bot, chat_id: int, caption: str, reply_id: int = None) -> TelegramMessageArgs:
         return {'self': from_bot,
                 'chat_id': chat_id,
                 'caption': caption,
                 'reply_id': reply_id}
 
-    @staticmethod
-    def _get_human_users(user_list: Iterable[User]) -> Iterable[User]:
+    def _get_welcome_message(self, message: Message) -> str:
+        human_users: List[User] = self._get_human_users(message.new_chat_members)
+        response_text: str = ", ".join([user.name for user in human_users])
+        welcome_chat: str = config.NEW_USER_REPLY.format(new_member_names=response_text,
+                                                         chat_name=message.chat.title)
+        return welcome_chat
+
+    def _get_human_users(self, user_list: Iterable[User]) -> List[User]:
         return [user for user in user_list if not user.is_bot]
