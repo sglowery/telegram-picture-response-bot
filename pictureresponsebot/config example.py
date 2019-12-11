912b782d118b9e347e9419657f8b4d363ac66f2c
sglowery <stephen.g.lowery@gmail.com>
2019-12-10 19:59:51 -0700

    remove old source code config file

--- pictureresponsebot/config example.py
+++
@@ -1,8 +0,0 @@
-NEW_USER_REPLY = "Hi {new_member_name}! Welcome to {chat_name}. Enjoy a complimentary image, on the house."
-COMMAND_REPLY = "Why hello there. Have you seen this picture? Now you have."
-SEND_PICTURE_COMMAND = "picture"
-
-IMAGE_DIRECTORY = "pictureresponsebot/assets/"
-IMAGE_NAME = "response.jpg"
-
-TOKEN = "telegram bot token here"
